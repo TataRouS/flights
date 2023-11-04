233266e4845a13534548d8e6de5be2ea2299b892
Nata Kuznetsova <natanavky@icloud.com>
2023-11-04 10:47:07 +0300

    bugfix Cells.

--- flights/flights/Arrival/ArrivalCell.swift
+++ flights/flights/Arrival/ArrivalCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class ArrivalCell: UITableViewCell {
     
-    private var time: UILabel = {
+    private var text4: UILabel = {
         let label = UILabel()
         label.text = "Description"
         label.textColor = .black
@@ -43,11 +43,11 @@ final class ArrivalCell: UITableViewCell {
         let number = departure.thread.number
         let title = departure.thread.title
         let vehicle = departure.thread.vehicle
-        let time = departure.departure
+        let time = departure.arrival ?? "время не известно"
         text1.text = "\(number)"
         text2.text = "\(title)"
         text3.text = "\(vehicle)"
-      //  time.text = "\(time)"
+        text4.text = "\(time)"
     }
     
         
@@ -61,40 +61,41 @@ final class ArrivalCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupViews() {
-        contentView.addSubview(time)
-        contentView.addSubview(text1)
-        contentView.addSubview(text2)
-        contentView.addSubview(text3)
-        setupConstraints()
-    }
-    
-    private func setupConstraints() {
-        time.translatesAutoresizingMaskIntoConstraints = false
-        text1.translatesAutoresizingMaskIntoConstraints = false
-        text2.translatesAutoresizingMaskIntoConstraints = false
-        text3.translatesAutoresizingMaskIntoConstraints = false
+  
+        private func setupViews() {
+            contentView.addSubview(text4)
+            contentView.addSubview(text1)
+            contentView.addSubview(text2)
+            contentView.addSubview(text3)
+            setupConstraints()
+        }
         
-        
-        NSLayoutConstraint.activate([
-            time.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            time.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 10),
-           time.heightAnchor.constraint(equalToConstant: 50),
-            time.widthAnchor.constraint(equalTo: time.heightAnchor),
-          time.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+        private func setupConstraints() {
+            text4.translatesAutoresizingMaskIntoConstraints = false
+            text1.translatesAutoresizingMaskIntoConstraints = false
+            text2.translatesAutoresizingMaskIntoConstraints = false
+            text3.translatesAutoresizingMaskIntoConstraints = false
             
-            text1.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            text1.trailingAnchor.constraint(equalTo: time.leadingAnchor, constant: 30),
-            text1.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
             
-            text2.topAnchor.constraint(equalTo: text1.bottomAnchor, constant: 10),
-            text2.trailingAnchor.constraint(equalTo: time.leadingAnchor, constant: 30),
-            text2.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-            
-            text3.topAnchor.constraint(equalTo: text2.bottomAnchor, constant: 10),
-            text3.trailingAnchor.constraint(equalTo: time.leadingAnchor, constant: 30),
-            text3.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10)
-            
+            NSLayoutConstraint.activate([
+                text4.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                text4.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 10),
+                text4.heightAnchor.constraint(equalToConstant: 80),
+                text4.widthAnchor.constraint(equalTo: text4.heightAnchor),
+                text4.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+                
+                text1.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+                text1.trailingAnchor.constraint(equalTo: text4.leadingAnchor, constant: 30),
+                text1.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+                
+                text2.topAnchor.constraint(equalTo: text1.bottomAnchor, constant: 10),
+                text2.trailingAnchor.constraint(equalTo: text4.leadingAnchor, constant: 30),
+                text2.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+                
+                text3.topAnchor.constraint(equalTo: text2.bottomAnchor, constant: 10),
+                text3.trailingAnchor.constraint(equalTo: text4.leadingAnchor, constant: 30),
+                text3.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10)
+                
         ])
     }
     
